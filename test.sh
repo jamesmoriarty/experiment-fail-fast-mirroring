596b77a85fb3f14d0a8404042d21f4235086bea2
James Moriarty <james.moriarty@news.com.au>
2018-01-30 11:20:23 +1100

    wip

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -x
+set -e
+
+docker-compose down
+docker-compose build
+docker-compose up &
+
+while ! nc -z localhost 8081; do   
+  sleep 0.1 # wait for 1/10 of the second before check again
+done
+
+wrk -d 30 http://localhost:8081/ > results/$(date +"%Y%m%d-%H%M%S.txt")
+
